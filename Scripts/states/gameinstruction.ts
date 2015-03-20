e3e0be50fa0f1106c8bdb5dc77182e7434937af8
lakinpatel <lakinpatel@yahoo.com>
2015-03-20 19:53:05 -0400

    Few Changes & Instructions Added to Project

--- Scripts/states/gameinstruction.ts
+++ Scripts/states/gameinstruction.ts
@@ -12,13 +12,14 @@ module states {
         // Game Objects 
         public game: createjs.Container;
         public ocean: objects.Ocean;
-        public mailPilotLabel: objects.Label;
+        public headingLabel: objects.Label;
+        public instructionsLabel: objects.Label;
         public playButton: objects.Button;
         public play: boolean = false;
 
         // CONSTRUCTOR ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         constructor() {
-            
+
 
             // Instantiate Game Container
             this.game = new createjs.Container();
@@ -27,16 +28,27 @@ module states {
             this.ocean = new objects.Ocean();
             this.game.addChild(this.ocean);
 
-            //Game Over Label
-            this.mailPilotLabel = new objects.Label(320, 40, "GAME INSTRUCTIONS");
-            this.mailPilotLabel.font = "60px Consolas";
-            this.mailPilotLabel.regX = this.mailPilotLabel.getMeasuredWidth() * 0.5;
-            this.mailPilotLabel.regY = this.mailPilotLabel.getMeasuredLineHeight() * 0.5;
-            this.game.addChild(this.mailPilotLabel);
+            //Heading Label
+            this.headingLabel = new objects.Label(300, 40, "GAME INSTRUCTIONS");
+            this.headingLabel.font = "40px Century";
+            this.headingLabel.regX = this.headingLabel.getMeasuredWidth() * 0.5;
+            this.headingLabel.regY = this.headingLabel.getMeasuredLineHeight() * 0.5;
+            this.game.addChild(this.headingLabel);
 
+            //Instructions Label
+            this.instructionsLabel = new objects.Label(300, 50, this.getInstructions());
+            this.instructionsLabel.font = "20px Century";
+            //Setting up postion of the instruction label
+            this.instructionsLabel.regX = this.headingLabel.getMeasuredWidth() * 0.5;
+            this.instructionsLabel.regY = this.headingLabel.getMeasuredLineHeight() * 0.5;
+            var w = (this.instructionsLabel.getMeasuredWidth()) * this.instructionsLabel.scaleX;
+            var h = (this.instructionsLabel.getMeasuredHeight()) * this.instructionsLabel.scaleY;
+            this.instructionsLabel.textAlign = 'left'
+            this.instructionsLabel.lineWidth = 500;
+            this.game.addChild(this.instructionsLabel);
 
             //Play Button
-            this.playButton = new objects.Button(320, 280, "playButton");
+            this.playButton = new objects.Button(320, 350, "playButton");
             this.playButton.on("click", this.playClicked, this);
 
             this.game.addChild(this.playButton);
@@ -47,7 +59,7 @@ module states {
 
         public playClicked() {
             this.play = true;
-            createjs.Sound.removeSound("assets/audio/game.mp3", "");
+            createjs.Sound.removeSound("assets/audio/game.mp3", ""); // remove game sound on state stage
         }
 
 
@@ -59,12 +71,18 @@ module states {
             if (this.play) {
                 this.game.removeAllChildren();
                 stage.removeChild(this.game);
-                currentState = constants.PLAY_STATE;
+                currentState = constants.PLAY_STATE; // change game state to play
                 stateChanged = true;
             }
 
             stage.update(); // Refreshes our stage
 
         } // Update Method
+
+        /* Method to change instructions*/
+        public getInstructions() {
+            return "\n\n\n\n1. Move your Mouse Up and Down to Move the \n    Mario, only up and down is allowed \n\n" +
+                "2. Avoid to Touch with the Enemies, otherwise you \n    will lose one life. \n\n3. Catch the small Ice Cream, each ice-cream\n    gives you 100 points ";
+        }
     }
 } 
